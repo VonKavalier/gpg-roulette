0fc0fc8fd8aef6cb50c253d159ebe326469274d3
VonKavalier <von@disroot.org>
2019-10-07 07:44:57 +0000

    Small updates, mostly indentation

--- gpg-roulette.py
+++ gpg-roulette.py
@@ -7,37 +7,37 @@ import operator
 import gnupg
 
 def get_three_rand_characters():
-	characters = ""
-	for i in range(3):
-		choice = random.randrange(0,2)
-		if choice == 0:
-			characters = characters + random.choice(string.ascii_lowercase)
-		else:
-			characters = characters + str(random.randrange(1,10))
-	return characters
+    characters = ""
+    for i in range(3):
+        choice = random.randrange(0,2)
+        if choice == 0:
+            characters = characters + random.choice(string.ascii_lowercase)
+        else:
+            characters = characters + str(random.randrange(1,10))
+    return characters
 
 def get_json(url_param):
-	url = url_param
-	response = urllib2.urlopen(url)
-	data = response.read()
-	values = json.loads(data)
-	return values
+    url = url_param
+    response = urllib2.urlopen(url)
+    data = response.read()
+    values = json.loads(data)
+    return values
 
 def get_random_login():
-	values = get_json("https://keybase.io/_/api/1.0/user/autocomplete.json?q="+get_three_rand_characters())
-	while len(values["completions"]) == 0 :
-		values = get_json("https://keybase.io/_/api/1.0/user/autocomplete.json?q="+get_three_rand_characters())
-	user = random.choice(values["completions"])
-	return user["components"]["username"]["val"]
+    values = get_json("https://keybase.io/_/api/1.0/user/autocomplete.json?q="+get_three_rand_characters())
+    while len(values["completions"]) == 0 :
+        values = get_json("https://keybase.io/_/api/1.0/user/autocomplete.json?q="+get_three_rand_characters())
+    user = random.choice(values["completions"])
+    return user["components"]["username"]["val"]
 
 def get_gpg_key():
-	key = requests.get("https://keybase.io/"+get_random_login()+"/pgp_keys.asc") 
-	#key_file = open("pgp_keys.asc","w")
-	while key.status_code != 200:
-		key = requests.get("https://keybase.io/"+get_random_login()+"/pgp_keys.asc")
-	#key_file.write(key.text)
-	#key_file.close()
-	return key.text
+    key = requests.get("https://keybase.io/"+get_random_login()+"/pgp_keys.asc") 
+    #key_file = open("pgp_keys.asc","w")
+    while key.status_code != 200:
+        key = requests.get("https://keybase.io/"+get_random_login()+"/pgp_keys.asc")
+    #key_file.write(key.text)
+    #key_file.close()
+    return key.text
 
 gpg_home = "~/.gnupg"
 
@@ -51,3 +51,7 @@ afile = open("message.txt", "rb")
 encrypted_ascii_data = gpg.encrypt_file(afile, import_result.fingerprints, always_trust=True, output=savefile)
 afile.close()
 str(gpg.delete_keys(import_result.fingerprints[0]))
+
+encrypted_file = open("message.asc","r")
+print encrypted_file.read()
+encrypted_file.close()
